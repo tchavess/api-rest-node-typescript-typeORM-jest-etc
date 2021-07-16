8ea9e4114ff77728829575ec011b57cbeb1d71a1
Thiago Chaves <thiago.chaves@calindra.com.br>
2021-07-16 17:06:26 -0300

    UserTokensRepository.ts

--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import User from '../typeorm/entities/User';
+import User from '../entities/User';
 
 @EntityRepository(User)
 class UsersRepository extends Repository<User> {
