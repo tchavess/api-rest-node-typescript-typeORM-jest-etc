55f99a155bfc44d13fca263ad212fce4d44dab60
Thiago Chaves <thiago.chaves@calindra.com.br>
2021-07-28 14:09:54 -0300

    typeorm/entities

--- src/modules/orders/services/ShowOrderService.ts
+++ src/modules/orders/services/ShowOrderService.ts
@@ -4,21 +4,21 @@ import Order from '../typeorm/entities/Order';
 import OrdersRepository from '../typeorm/repositories/OrdersRepository';
 
 interface IRequest {
-  id: string;
+    id: string;
 }
 
 class ShowOrderService {
-  public async execute({ id }: IRequest): Promise<Order> {
-    const ordersRepository = getCustomRepository(OrdersRepository);
+    public async execute({ id }: IRequest): Promise<Order> {
+        const ordersRepository = getCustomRepository(OrdersRepository);
 
-    const order = await ordersRepository.findById(id);
+        const order = await ordersRepository.findById(id);
 
-    if (!order) {
-      throw new AppError('Order not found.');
+        if (!order) {
+            throw new AppError('Order not found.');
+        }
+
+        return order;
     }
-
-    return order;
-  }
 }
 
 export default ShowOrderService;
