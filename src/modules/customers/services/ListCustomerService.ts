ca7316e29234aeb9c32e9ab308580bddcebb4edf
Thiago Chaves <thiago.chaves@calindra.com.br>
2021-07-28 11:59:58 -0300

    customer routes

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -16,9 +16,8 @@ class ListCustomerService {
     public async execute(): Promise<IPaginateCustomer> {
         const customersRepository = getCustomRepository(CustomersRepository);
 
-        const customers = await customersRepository
-            .createQueryBuilder()
-            .paginate();
+        const customers =
+            await customersRepository.createQueryBuilder.paginate();
 
         return customers as IPaginateCustomer;
     }
